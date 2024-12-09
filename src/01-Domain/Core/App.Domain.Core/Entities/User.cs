608492be058f30616354e1aa772c5cb90c04faa2
keyvan68 <keyvanhafezy1368@gmail.com>
2024-12-09 16:56:21 +0330

    change user table add image

--- src/01-Domain/Core/App.Domain.Core/Entities/User.cs
+++ src/01-Domain/Core/App.Domain.Core/Entities/User.cs
@@ -12,6 +12,7 @@ namespace App.Domain.Core.Entities
         public int UserId { get; set; }
         public string Name { get; set; }
         public string Family { get; set; }
+        public string? ImageUrl { get; set; }
         public  UserDetails UserDetails { get; set; }
     }
 }
---
+++ src/02-Infrastructures/App.Infrastructures.Db.SqlServer.Ef/Migrations/20241209132453_addImage.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace App.Infrastructures.Db.SqlServer.Ef.Migrations
+{
+    public partial class addImage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "User");
+        }
+    }
+}
